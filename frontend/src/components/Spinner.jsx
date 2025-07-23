d7d43e379705066575de4d551f3537fd0d79cd9c
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2025-07-23 15:58:15 +0300

    update spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,16 +1,20 @@
-import { RotatingLines } from 'react-loader-spinner';
-
-const Spinner = ({ color, width, height }) => (
-  <div className="flex justify-center items-center h-full">
-    <RotatingLines
-      radius='9'
-      wrapperStyle
-      wrapperClass
-      width={width || '32'}
-      height={height || '32'}
-      strokeColor={color || 'grey'}
-      ariaLabel='three-dots-loading'
-    />
+const Spinner = () => (
+  <div role="status" className="flex justify-center py-8">
+    <svg className="animate-spin h-6 w-6 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle
+        r="10"
+        cx="12"
+        cy="12"
+        strokeWidth="4"
+        stroke="currentColor"
+        className="opacity-25"
+      ></circle>
+      <path
+        fill="currentColor"
+        className="opacity-75"
+        d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 00-8 8h4z"
+      ></path>
+    </svg>
   </div>
 );
 
